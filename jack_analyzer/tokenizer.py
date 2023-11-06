33ba8dbe7ac2f2dd6a7cd5cbb9f1cd4faf3bd5bc
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2023-11-06 16:55:29 -0600

    Update comment handling

--- jack_analyzer/tokenizer.py
+++ jack_analyzer/tokenizer.py
@@ -7,6 +7,8 @@ pieces of a file into valid Jack tokens
 from __future__ import annotations
 from collections import deque
 
+from constants import COMMENT, ML_COMMENT_START, ML_COMMENT_END
+
 
 def read_file(filename: str) -> list[str]:
     """Read a .jack file, and return it as a list of strings
@@ -23,19 +25,33 @@ def read_file(filename: str) -> list[str]:
         return contents
 
 
-def is_comment(line: str) -> bool:
+def is_single_comment(line: str) -> bool:
     """Return true if provided line is a .jack comment.
 
     Single-line are signifed by `//`.  Multi-line comments by `/* */` or `/** */`
-    and will likely be parsed differently
+    and are parsed differently because we can't necessarily just throw the whole line away
 
     Args:
         `line` (str): The unparsed .jack code line
 
     Returns:
-        `bool`: If the line is a comment or not
+        `bool`: If the line is a single-line comment or not
     """
-    return line.startswith("//")
+
+    return line.startswith(COMMENT)
+
+
+def is_full_ml_comment(line: str) -> bool:
+    """Returns true if provided line is a .jack multi-line comment
+
+    Args:
+        `line` (str): The unparsed .jack code line
+
+    Returns:
+        `bool`: If the line is a multi-line comment or not
+    """
+
+    return line.startswith(ML_COMMENT_START) and line.endswith(ML_COMMENT_END)
 
 
 def parse_file(filename: str) -> deque:
@@ -47,4 +63,16 @@ def parse_file(filename: str) -> deque:
     Returns:
         `deque`: A deque (to be used as a stack) representing the tokenized file
     """
+    stack = deque()
+    contents = read_file(filename)
+    active_comment = False
+
+    for line in contents:
+        # if a single-line comment or the whole line is a multi-line comment
+        # , dispose of it/do nothing
+        if is_single_comment(line) or is_full_ml_comment(line):
+            continue
+        # TODO: Handle actual mutli-line comments, including ones that start/end around actual tokens
+
     raise NotImplementedError
+    return stack
