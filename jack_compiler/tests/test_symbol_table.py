1f45810d5dc4d3da5b559ac92f4eaa42b5bad871
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-05-11 14:10:05 -0500

    test(symbol-table): Add test for >1 class var dec on 1 line

--- jack_compiler/tests/test_symbol_table.py
+++ jack_compiler/tests/test_symbol_table.py
@@ -75,3 +75,56 @@ def test_class_var_dec_output(test_class_var_tokens, compiled_class_var_tokens) 
     engine = CompilationEngine("test", tokens=test_class_var_tokens)
     engine.compile_class_var_dec()
     assert engine._compiled_tokens == compiled_class_var_tokens
+
+
+@fixture
+def test_multi_class_var_tokens() -> list[str]:
+    return [
+        "<keyword> static </keyword>\n",
+        "<keyword> int </keyword>\n",
+        "<identifier> x </keyword>\n",
+        "<symbol> , </symbol>\n",
+        "<identifier> y </keyword>\n",
+        "<symbol> , </symbol>\n",
+        "<identifier> z </keyword>\n",
+        "<symbol> ; </symbol>\n",
+    ]
+
+
+@fixture
+def compiled_multi_class_var_tokens() -> deque[str]:
+    return deque(
+        [
+            "<classVarDec>\n",
+            "<keyword> static </keyword>\n",
+            "<keyword> int </keyword>\n",
+            "<identifier category='static' index=0 usage='declared'> x </identifier>\n",
+            "<symbol> , </symbol>\n",
+            "<identifier category='static' index=1 usage='declared'> y </identifier>\n",
+            "<symbol> , </symbol>\n",
+            "<identifier category='static' index=2 usage='declared'> z </identifier>\n",
+            "<symbol> ; </symbol>\n",
+            "</classVarDec>\n",
+        ]
+    )
+
+
+@fixture
+def test_multi_class_var_symbol_table():
+    x = Identifier(name="x", data_type="int", category="static", index=0)
+    y = Identifier(name="y", data_type="int", category="static", index=1)
+    z = Identifier(name="z", data_type="int", category="static", index=2)
+    table = SymbolTable(
+        class_table={"x": x, "y": y, "z": z},
+        indexes={"static": 3, "field": 0, "arg": 0, "var": 0},
+    )
+    yield table
+    del table
+
+
+def test_multi_class_var_dec_output(
+    test_multi_class_var_tokens, compiled_multi_class_var_tokens
+) -> None:
+    engine = CompilationEngine("test", tokens=test_multi_class_var_tokens)
+    engine.compile_class_var_dec()
+    assert engine._compiled_tokens == compiled_multi_class_var_tokens
